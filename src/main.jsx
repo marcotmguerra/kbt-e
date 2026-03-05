763ed2e8a813cc53c85a26f04f3f6f7021cef11f
marcotmguerra <mguerra155@gmail.com>
2026-03-05 12:51:19 -0300

    assets e css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './styles/base.css' // Importe seu CSS global aqui
+
+import './styles/base.css'
+import './styles/admin.css'
+import './styles/professor.css'
+import './styles/pos-aula.css'
+import './styles/detalhe.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
